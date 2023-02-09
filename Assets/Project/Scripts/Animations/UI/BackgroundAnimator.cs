131f38df84b378517d1d5b26dd08ac4f209e326a
gama <cgama@gazeus.com>
2023-02-09 13:30:52 -0500

    First commit

---
+++ Assets/Project/Scripts/Animations/UI/BackgroundAnimator.cs
@@ -0,0 +1,29 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class BackgroundAnimator : MonoBehaviour
+{
+    [SerializeField] private Image image;
+
+    [Range(0.1f, 10f)]
+    [SerializeField] private float loopTime = 3f;
+
+    private void OnValidate()
+    {
+        if (image == null)
+            this.image = base.GetComponent<Image>();
+    }
+
+    private void Start()
+    {
+        this.PlayColorAnimation();
+    }
+
+    private void PlayColorAnimation()
+    {
+        this.image.DOColor(Random.ColorHSV(), this.loopTime)
+            .OnComplete(this.PlayColorAnimation);
+    }
+}
